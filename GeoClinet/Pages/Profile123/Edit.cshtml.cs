1153652efeb7c63b584e8be5825f36f21a47acf7
khanh Tuong <146071055+KhanhTuong3002@users.noreply.github.com>
2024-05-20 17:19:06 +0700

    add profile list

---
+++ GeoClinet/Pages/Profile123/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Entites;
+using DataAccess;
+
+namespace GeoClinet.Pages.Profile123
+{
+    public class EditModel : PageModel
+    {
+        private readonly DataAccess.GeoTycoonDbcontext _context;
+
+        public EditModel(DataAccess.GeoTycoonDbcontext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Profile Profile { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var profile =  await _context.Profiles.FirstOrDefaultAsync(m => m.Id == id);
+            if (profile == null)
+            {
+                return NotFound();
+            }
+            Profile = profile;
+           ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Profile).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProfileExists(Profile.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProfileExists(string id)
+        {
+            return _context.Profiles.Any(e => e.Id == id);
+        }
+    }
+}
