653b3bcc6c1807c853b75449172ac912b8ee03f2
haoqiong1506 <robinolvia0602@gmail.com>
2024-05-22 15:47:32 +0700

    update dialog

--- GeoClinet/Pages/setdetail/Index.cshtml.cs
+++ GeoClinet/Pages/setdetail/Index.cshtml.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using BusinessObject.Entites;
 using DataAccess;
+using BusinessObject.Entites;
 
-namespace GeoClinet.Pages.setdetail
+namespace GeoClient.Pages.setdetail
 {
     public class IndexModel : PageModel
     {
@@ -19,28 +19,66 @@ namespace GeoClinet.Pages.setdetail
             _context = context;
         }
 
-        public IList<SetQuestionDetail> SetQuestionDetail { get;set; } = default!;
+        public IList<SetQuestionDetail> SetQuestionDetails { get; set; } = new List<SetQuestionDetail>();
+        public IList<SetQuestion> SetQuestions { get; set; } = new List<SetQuestion>();
+        public IList<Question> Questions { get; set; } = new List<Question>();
+
+        [BindProperty]
+        public SetQuestionDetail SetQuestionDetail { get; set; }
 
         public async Task OnGetAsync()
         {
-            SetQuestionDetail = await _context.SetQuestionDetails
-                .Include(s => s.Question)
-                .Include(s => s.SetQuestion).ToListAsync();
+            SetQuestionDetails = await _context.SetQuestionDetails.Include(d => d.SetQuestion).Include(d => d.Question).ToListAsync();
+            SetQuestions = await _context.SetQuestions.ToListAsync();
+            Questions = await _context.Questions.ToListAsync();
         }
 
-        public async Task<IActionResult> OnPostDeleteAsync(string id)
+        public async Task<IActionResult> OnPostAddDetailAsync()
         {
-            var questionToDelete = await _context.SetQuestionDetails.FindAsync(id);
+            _context.SetQuestionDetails.Add(SetQuestionDetail);
+            await _context.SaveChangesAsync();
+            return RedirectToPage();
+        }
 
-            if (questionToDelete == null)
+        public async Task<IActionResult> OnPostEditDetailAsync(string id)
+        {
+            _context.Attach(SetQuestionDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SetQuestionDetailExists(SetQuestionDetail.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteDetailAsync(string id)
+        {
+            var detailToDelete = await _context.SetQuestionDetails.FindAsync(id);
+            if (detailToDelete == null)
             {
                 return NotFound();
             }
 
-            _context.SetQuestionDetails.Remove(questionToDelete);
+            _context.SetQuestionDetails.Remove(detailToDelete);
             await _context.SaveChangesAsync();
-
             return RedirectToPage();
+        }
+
+        private bool SetQuestionDetailExists(string id)
+        {
+            return _context.SetQuestionDetails.Any(e => e.Id == id);
         }
     }
 }
