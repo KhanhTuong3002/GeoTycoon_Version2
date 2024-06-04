5da4f03df03cbb70b3c3861217a51474808e4de5
Sururing <thanhdongacong1012@gmail.com>
2024-06-04 18:17:16 +0700

    validation datetime

--- GeoClinet/Pages/Questionsss/Create.cshtml.cs
+++ GeoClinet/Pages/Questionsss/Create.cshtml.cs
@@ -56,6 +56,12 @@ namespace GeoClinet.Pages.Questionsss
                 ModelState.AddModelError("Question.Option3", "Options cannot be duplicated.");
                 return Page();
             }
+            // Validate the Published date
+            if (Question.Published > DateTime.Now)
+            {
+                ModelState.AddModelError("Question.Published", "Published date cannot be in the future.");
+                return Page();
+            }
             Question.UserId = currentUser?.Id;
             _context.Questions.Add(Question);
             await _context.SaveChangesAsync();
