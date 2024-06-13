a9df7f1157d353c9f4cd4261151a11b5603cfa1c
haoqiong1506 <robinolvia0602@gmail.com>
2024-06-14 00:27:58 +0700

    update homepage

--- GeoClinet/src/tailwind.config.js
+++ GeoClinet/src/tailwind.config.js
@@ -1,15 +1,36 @@
+﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["**/*.cshtml"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Poppins', 'Rubik', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Helvetica Neue', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+            },
+            colors: {
+                bodyColor: '#2F281E',
+                backgroundColor: '#FFFEFE',
+            },
+        },
     },
+    plugins: [require("daisyui")],
     daisyui: {
         themes: [
-            "retro",
+            {
+                "ayu-light": {
+                    "primary": "#747DEF",
+                    "secondary": "#544837",
+                    "accent": "#ffbf00",
+                    "neutral": "#F0F4F9",
+                    "base-100": "#FCFCFC",
+                    "base-200": "#F3F4F5",
+                    "base-300": "#ACB6BF",
+                    "info": "#567592",
+                    "success": "#79B93C",
+                    "warning": "#FF9900",
+                    "error": "#DF6951",
+                },
+            },
         ],
     },
-    plugins: [
-        require('daisyui'),
-    ],
-
 }
+
