5469197b8cad6198396eed2fda7b216c3f85b9a7
haoqiong1506 <robinolvia0602@gmail.com>
2024-08-01 23:12:14 +0700

    HowToPlay

---
+++ GeoClinet/Pages/HowToPlay.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GeoClinet.Pages
+{
+    public class HowToPlayModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
